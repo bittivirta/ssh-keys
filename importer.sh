966e39d6877c7eb57934791bec3e35eaec76d925
korhox <juuso.korhonen@bittivirta.fi>
2023-07-15 13:44:10 +0300

    feat: :sparkles: Update colors

--- importer.sh
+++ importer.sh
@@ -2,24 +2,12 @@
 
 # Color Variables
 Color_Off='\033[0m'       # Text Reset
-
-Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
 Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
-bBlack='\033[1;30m'       # Bold Black
+Green='\033[0;32m'        # Green
 bRed='\033[1;31m'         # Bold Red
-bGreen='\033[1;32m'       # Bold Green
-bYellow='\033[1;33m'      # Bold Yellow
 bBlue='\033[1;34m'        # Bold Blue
-bPurple='\033[1;35m'      # Bold Purple
-bCyan='\033[1;36m'        # Bold Cyan
-bWhite='\033[1;37m'       # Bold White
+bGreen='\033[1;32m'       # Bold Green
 
 # Get the users.json file
 USERS=$(curl -s https://raw.githubusercontent.com/bittivirta/ssh-keys/main/users.json)
@@ -32,13 +20,13 @@ function checkDepedencies() {
     # Check if jq is installed
     if ! command -v jq &> /dev/null
     then
-        echo -e "${bRed}Error: ${Red}jq is not installed!${Color_Off}"
-        echo -e "${blue}Would you like to install it? (y/n)${Color_Off}"
+        echo -e "${bRed}Error: ${Red}jq is not installed!"
+        echo -e "${blue}Would you like to install it? (y/n)"
         read install
         if [ "$install" == "y" ]; then
-            sudo apt update && sudo apt install jq || echo -e "${bRed}Error: ${Red}Failed to install jq!${Color_Off}"
+            sudo apt update && sudo apt install jq || echo -e "${bRed}Error: ${Red}Failed to install jq!"
         else
-            echo -e "${bRed}The jq package is required to run this script. Aborting...${Color_Off}"
+            echo -e "${bRed}The jq package is required to run this script. Aborting..."
             exit 1
         fi
     fi
@@ -90,7 +78,7 @@ function getUserKey() {
         getUserKeyByUsername
     fi
     if [ $? -eq 0 ]; then
-        echo -e "${bRed}Error: ${Red}User not found by ID or username!${Color_Off}"
+        echo -e "${bRed}Error: ${Red}User not found by ID or username!"
         exit 1
     fi
 }
@@ -101,26 +89,26 @@ function askConfirmation() {
     read confirm
 
     if [ "$confirm" != "y" ]; then
-        echo -e "${bRed}Aborting...${Color_Off}"    §
+        echo -e "${bRed}Aborting..."    §
         exit 1
     fi
 }
 
 function importKey() {
     # Import the key
-    echo -e "${bBlue}Importing the key...${Color_Off}"
+    echo -e "${bBlue}Importing the key..."
 
     grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || echo "$SSHKEY" >> ~/.ssh/authorized_keys && SUCCESS=1
     grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || SUCCSS=0
 
     # Check if the key was imported
     if [ "$SUCCESS" != 1 ]; then
-        echo -e "${bRed}Error: ${Red}Failed to import the key!${Color_Off}"
+        echo -e "${bRed}Error: ${Red}Failed to import the key!"
         exit 1
     fi
 
     # Success message
-    echo -e "${bGreen}Key imported successfully!${Color_Off}"
+    echo -e "${bGreen}Key imported successfully!"
 }
 
 function warnRemoval() {
