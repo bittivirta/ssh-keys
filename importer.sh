c04381ff0aed3884323b503cefa5edb26e20c610
korhox <juuso.korhonen@bittivirta.fi>
2023-07-15 12:56:40 +0300

    feat: :recycle: Make code reuseable by deviding it to funcitons

--- importer.sh
+++ importer.sh
@@ -24,53 +24,101 @@ bWhite='\033[1;37m'       # Bold White
 # Get the users.json file
 USERS=$(curl -s https://raw.githubusercontent.com/bittivirta/ssh-keys/main/users.json)
 
-# Welcome message
-echo -e "${bBlue}Welcome to the Bittivirta Staff Key importer!${Color_Off}\n"
+function welcome() {
+    echo -e "${bBlue}Welcome to the Bittivirta Staff Key importer!${Color_Off}\n"
+}
+
+function listUsers() {
+    echo -e "${bBlue}Available users:${Color_Off}\n"
+    echo "$USERS" | jq -r '.[] | "\(.id). \(.username), \(.name)"' | tr -d '”'
+}
+
+function askUserID() {
+    echo -e "\n"
+    echo -ne "${Blue}Enter the user ID to import:${Color_Off} "
+    read -r key
+    KEY=$key
+}
+
+function getUserKeyByID() {
+    echo $key
+    # Check if the key exists
+    USERNAME=$(echo "$USERS" | jq -r ".[] | select(.id == $KEY) | .username")
 
 
-# List the users
-echo -e "${bBlue}Available users:${Color_Off}\n"
-echo "$USERS" | jq -r '.[] | "\(.id). \(.username), \(.name)"' | tr -d '”'
+    if [ "$USERNAME" != "" ]; then
+        return 0
+    fi
 
-# Ask for the user ID
-echo -e "\n"
-echo -ne "${Blue}Enter the user ID to import:${Color_Off} "
-read key
+    # Get the key
+    SSHKEYURL=$(echo "$USERS" | jq -r ".[] | select(.id == $KEY) | .keys")
+    SSHKEY=$(curl -s "$SSHKEYURL")
+}
 
-# Check if the key exists
-USERNAME=$(echo "$USERS" | jq -r ".[] | select(.id == $key) | .username")
-echo -e "${Blue}Downloading ${bBlue}$USERNAME${Blue}'s key..."
+function getUserKeyByUsername() {
+    # Check if the key exists
+    USERNAME=$(echo "$USERS" | jq -r ".[] | select(.username == \"$KEY\") | .username")
 
-# Get the key
-SSHKEYURL=$(echo "$USERS" | jq -r ".[] | select(.id == $key) | .keys")
-SSHKEY=$(curl -s "$SSHKEYURL")
-SSHKEY="$SSHKEY bittivirta-key-$USERNAME"
+    if [ "$USERNAME" != "" ]; then
+        return 0
+    fi
 
-# Ask for confirmation
-echo -e "${Blue}The key will be imported to the current user's authorized_keys file (~/.ssh/authorized_keys).${Color_Off}\n"
-echo -ne "${bBlue}Continue? (y/n)${Color_Off} "
-read confirm
+    # Get the key
+    SSHKEYURL=$(echo "$USERS" | jq -r ".[] | select(.username == \"$KEY\") | .keys")
+    SSHKEY=$(curl -s "$SSHKEYURL")
+}
 
-if [ "$confirm" != "y" ]; then
-    echo -e "${bRed}Aborting...${Color_Off}"    §
-    exit 1
-fi
+function getUserKey() {
+    getUserKeyByID
+    if [ $? -eq 0 ]; then
+        getUserKeyByUsername
+    fi
+    if [ $? -eq 0 ]; then
+        echo -e "${bRed}Error: ${Red}User not found by ID or username!${Color_Off}"
+        exit 1
+    fi
+}
 
-# Import the key
-echo -e "${bBlue}Importing the key...${Color_Off}"
+function askConfirmation() {
+    echo -e "${Blue}The key will be imported to the current user's authorized_keys file (~/.ssh/authorized_keys).${Color_Off}\n"
+    echo -ne "${bBlue}Continue? (y/n)${Color_Off} "
+    read confirm
 
-grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || echo "$SSHKEY" >> ~/.ssh/authorized_keys && SUCCESS=1
-grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || SUCCSS=0
+    if [ "$confirm" != "y" ]; then
+        echo -e "${bRed}Aborting...${Color_Off}"    §
+        exit 1
+    fi
+}
 
-# Check if the key was imported
-if [ "$SUCCESS" != 1 ]; then
-    echo -e "${bRed}Error: ${Red}Failed to import the key!${Color_Off}"
-    exit 1
-fi
+function importKey() {
+    # Import the key
+    echo -e "${bBlue}Importing the key...${Color_Off}"
 
-# Success message
-echo -e "${bGreen}Key imported successfully!${Color_Off}"
+    grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || echo "$SSHKEY" >> ~/.ssh/authorized_keys && SUCCESS=1
+    grep -qxF "$SSHKEY" ~/.ssh/authorized_keys || SUCCSS=0
 
-# Warning message
-echo -e "\n${bRed}Warning: ${Red}Please remember to remove the key from the file when it is no longer needed!"
-echo -e "         ${Red}If the key is not removed and the key is compromised, the attacker can access the server!"
+    # Check if the key was imported
+    if [ "$SUCCESS" != 1 ]; then
+        echo -e "${bRed}Error: ${Red}Failed to import the key!${Color_Off}"
+        exit 1
+    fi
+
+    # Success message
+    echo -e "${bGreen}Key imported successfully!${Color_Off}"
+}
+
+function warnRemoval() {
+    echo -e "\n${bRed}Warning: ${Red}Please remember to remove the key from the file when it is no longer needed!"
+    echo -e "         ${Red}If the key is not removed and the key is compromised, the attacker can access the server!"
+}
+
+function main() {
+    welcome
+    listUsers
+    askUserID
+    getUserKey
+    askConfirmation
+    importKey
+    warnRemoval
+}
+main
